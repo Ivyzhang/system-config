fdd0c98941a06749a4e126b64332b56748b244cd
Isaac Beckman <isaacb@il.ibm.com>
2015-11-01 14:43:37 +0200

    fix site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,5 @@
-    secret_key          => hiera('grafana_secret_key'),
-# Node-OS: trusty
-node 'health.openstack.org' {
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [80, 443],
-    sysadmins                 => hiera('sysadmins', []),
-  }
-  class { 'openstack_project::openstack_health_api':
-    subunit2sql_db_host => hiera('subunit2sql_db_host', 'localhost'),
-  }
-}
 
-
-
-# node-OS: precise
+#node-OS: precise
 node 'ci-puppet-master.openstacklocal' {
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [4505, 4506, 8140],
@@ -25,27 +12,16 @@ node 'ci-puppet-master.openstacklocal' {
 }
 
 node 'ci-zuul.openstacklocal' {
-    iptables_public_tcp_ports => [22],
-  }
-  class { 'openstack_project::subunit_worker':
-                                   '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 9200:9400 -s \1 -j ACCEPT')
-  $iptables_clients_rule = regsubst ($elasticsearch_clients,
-                                     '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 9200:9400 -s \1 -j ACCEPT')
-  $iptables_rule = flatten([$iptables_nodes_rule, $iptables_clients_rule])
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [22],
-    iptables_rules6           => $iptables_rule,
-    iptables_rules4           => $iptables_rule,
   class { 'openstack_project::zuul_prod':
     project_config_repo            => 'https://git.openstack.org/openstack-infra/project-config',
     gerrit_server                  => 'review.openstack.org',
     gerrit_user                    => 'jenkins',
-    gerrit_ssh_host_key            => hiera('gerrit_ssh_rsa_pubkey_contents'),
-    zuul_ssh_private_key           => hiera('zuul_ssh_private_key_contents'),
+    gerrit_ssh_host_key            => hiera('gerrit_ssh_rsa_pubkey_contents', 'XXX'),
+    zuul_ssh_private_key           => hiera('zuul_ssh_private_key_contents', 'XXX'),
     url_pattern                    => 'http://logs.openstack.org/{build.parameters[LOG_PATH]}',
     swift_authurl                  => 'https://identity.api.rackspacecloud.com/v2.0/',
     swift_user                     => 'infra-files-rw',
-    swift_key                      => hiera('infra_files_rw_password'),
+    swift_key                      => hiera('infra_files_rw_password', 'XXX'),
     swift_tenant_name              => hiera('infra_files_tenant_name', 'tenantname'),
     swift_region_name              => 'DFW',
     swift_default_container        => 'infra-files',
@@ -58,17 +34,6 @@ node 'ci-zuul.openstacklocal' {
     gearman_workers                => [
       'nodepool.openstack.org',
     ],
-  }  
-}
-
   }
 }
 
-# Node-OS: trusty
-node 'codesearch.openstack.org' {
-  class { 'openstack_project::server':
-    iptables_public_tcp_ports => [80],
-    sysadmins                 => hiera('sysadmins', []),
-  }
-  class { 'openstack_project::codesearch':
-    project_config_repo => 'https://git.openstack.org/openstack-infra/project-config',
