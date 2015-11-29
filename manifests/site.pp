94d9d399fda2de4c7819ccb47c2af0d5da3d1387
Isaac Beckman <isaacb@il.ibm.com>
2015-11-29 14:19:38 +0200

    update jenkins parameters in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,11 +41,10 @@ node 'ci-zuul.openstacklocal' {
 }
 
 node 'ci-jenkins-master.openstacklocal' {
-#  $group = "jenkins"
-#  $zmq_event_receivers = ['logstash.openstack.org',
-#                          'nodepool.openstack.org']
-#  $iptables_rule = regsubst ($zmq_event_receivers,
-#                             '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 8888 -s \1 -j ACCEPT')
+  $group = "jenkins"
+  $zmq_event_receivers = ['nodepool.openstack.org']
+  $iptables_rule = regsubst ($zmq_event_receivers,
+                             '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 8888 -s \1 -j ACCEPT')
   class { 'openstack_project::server':
     iptables_public_tcp_ports => [80, 443],
     iptables_rules6           => $iptables_rule,
@@ -53,13 +52,13 @@ node 'ci-jenkins-master.openstacklocal' {
     sysadmins                 => hiera('sysadmins', []),
     puppetmaster_server       => 'ci-puppet-master.openstacklocal',
   }
-#  class { 'openstack_project::jenkins':
-#    project_config_repo     => 'https://git.openstack.org/openstack-infra/project-config',
-#    jenkins_password        => hiera('jenkins_jobs_password'),
-#    jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents'),
-#    ssl_cert_file           => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-#    ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
-#    ssl_chain_file          => '',
-#  }
+  class { 'openstack_project::jenkins':
+    project_config_repo     => 'ssh://xivgit.xiv.ibm.com/git/host/devops/openstack_ci/project_config.git',
+    jenkins_password        => hiera('jenkins_jobs_password'),
+    jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents'),
+    ssl_cert_file           => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
+    ssl_chain_file          => '',
+  }
 }
 
