d330995a4ad0ad4d8ae4e23be0c5771656aaf9f3
Isaac Beckman <isaacb@il.ibm.com>
2015-12-28 14:09:50 +0200

    Typo

--- modules/openstack_project/manifests/puppetmaster.pp
+++ modules/openstack_project/manifests/puppetmaster.pp
@@ -204,7 +204,7 @@ class openstack_project::puppetmaster (
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    source  => '/usr/local/bin/puppet-inventory'
+    source  => '/usr/local/bin/puppet-inventory',
     replace => true,
     require => Vcsrepo['/opt/ansible'],
   }
